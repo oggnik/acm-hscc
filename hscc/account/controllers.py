13e6df2caee0dda0c983a0aa966af69364141ba7
Logan Gore <gorel@purdue.edu>
2016-01-29 17:32:20 -0500

    Lots of front-end work now

--- hscc/account/controllers.py
+++ hscc/account/controllers.py
@@ -11,7 +11,11 @@ from hscc import db
 from hscc import flash_form_errors
 
 from hscc.models import School
+from hscc.models import Team
 from hscc.models import User
+
+from hscc.account.forms import EditAccountForm
+from hscc.account.forms import JoinTeamForm
 
 
 mod_account = Blueprint('account', __name__, url_prefix='/account')
@@ -29,7 +33,19 @@ def home():
 @login_required
 def edit():
     """Return the account edit page"""
-    return render_template('account/edit.html')
+    form = EditAccountForm()
+
+    if form.validate_on_submit():
+        db.session.commit()
+        flash('Your changes have been saved successfully', 'alert-success')
+        return redirect(url_for('account.home'))
+    else:
+        flash_form_errors(form)
+        form.grade.data = current_user.grade
+        form.shirt_size.data = current_user.shirt_size
+        if current_user.allergies:
+            form.allergies_text.data = current_user.allergies.text
+        return render_template('account/edit.html', form=form)
 
 
 @mod_account.route('/my_school', methods=['GET'])
@@ -48,7 +64,7 @@ def school(id):
     """Return the school with id <id>"""
     sc = School.query.get(id)
     if not sc:
-        # TODO: Error: No school found
+        flash('Error: School not found.', 'alert-warning')
         return redirect(url_for('default.home'))
     return render_template('account/school.html', school=sc)
 
@@ -59,6 +75,27 @@ def schools():
     """Return a list of schools"""
     sc = School.query.all()
     return render_template('account/schools.html', schools=sc)
+
+
+@mod_account.route('/join_team/<int:id>', methods=['GET', 'POST'])
+@mod_account.route('/join_team/<int:id>/', methods=['GET', 'POST'])
+def join_team(id):
+    """Join the team with the given team id"""
+    t = Team.query.get(id)
+    if not t:
+        flash('Error: Team not found.', 'alert-warning')
+        return redirect(url_for('default.home'))
+
+    form = JoinTeamForm()
+    form.team_id.data = t.id
+
+    if form.validate_on_submit():
+        db.session.commit()
+        flash('Congrats! You are now a member of {}!'.format(t.name), 'alert-success')
+        return redirect(url_for('account.team', id=t.id))
+    else:
+        flash_form_errors(form)
+        return render_template('account/join_team.html', team=t, form=form)
 
 
 @mod_account.route('/my_team', methods=['GET'])
@@ -77,14 +114,14 @@ def team(id):
     """Return the team with id <id>"""
     t = Team.query.get(id)
     if not t:
-        # TODO: Error: No team found
+        flash('Error: Team not found.', 'alert-warning')
         return redirect(url_for('default.home'))
-    return render_teamplate('account/team.html', team=t)
+    return render_template('account/team.html', team=t)
 
 
-@mod_account.route('/teams/<int:school_id>', methods=['GET'])
-@mod_account.route('/teams/<int:school_id>/', methods=['GET'])
-def teams(school_id):
+@mod_account.route('/teams', methods=['GET'])
+@mod_account.route('/teams/', methods=['GET'])
+def teams():
     """Return a list of teams from the school with id <school_id>"""
     ts = Team.query.all()
     return render_template('account/teams.html', teams=ts)
